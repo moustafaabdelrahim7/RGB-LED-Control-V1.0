384feb38693af347d7543ec35fc968005bccb1c2
moustafaabdelrahim7 <128411699+moustafaabdelrahim7@users.noreply.github.com>
2023-06-15 10:29:30 +0200

    Update button.c

--- button.c
+++ button.c
@@ -1,16 +1,16 @@
 #include "button.h"
 
 
-uint8_t BUTTON_READ	(enu_port_pin_num_t enu_port_pin_num, enu_port_num_t enu_port_num)
+uint8_t BUTTON_READ	(enu_port_pin_num_t enu_a_port_pin_num, enu_port_num_t enu_a_port_num)
 {
 	uint8_t u8_l_status=False;
 
 	
 
-	if(!GPIO_READ_PIN(enu_port_num,enu_port_pin_num))
+	if(!GPIO_READ_PIN(enu_a_port_num,enu_a_port_pin_num))
 	{
 		u8_l_status=True;
-		while(!GPIO_READ_PIN(enu_port_num,enu_port_pin_num));	
+		while(!GPIO_READ_PIN(enu_a_port_num,enu_a_port_pin_num));	
 	}
 	
 	return u8_l_status;
@@ -18,10 +18,9 @@ uint8_t BUTTON_READ	(enu_port_pin_num_t enu_port_pin_num, enu_port_num_t enu_por
 	
 }
 /*********************************************************************************************/
-
-void BUTTON_INIT(const str_port_config_t*str_port_configPTR)
+void BUTTON_INIT(const str_port_config_t*str_a_port_configPTR)
 {
 
-	GPIO_INIT_PIN(str_port_configPTR);
+	GPIO_INIT_PIN(str_a_port_configPTR);
 
 }
